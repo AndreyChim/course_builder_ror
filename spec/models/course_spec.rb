85a8a858099621c8979880292ee0c93b9e882303
AndreyChim <chimitcyrenov@mail.ru>
2022-05-21 15:13:25 +0800

    add spec 3-5

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Course, type: :model do
   context "validations tests" do
     it "ensures the title is present" do
       course = Course.new(body: "Content of the body")
-      expect(course.valid?).to eq(true)
+      expect(course.valid?).to eq(false)
     end
 
     it "ensures the body is present" do
@@ -15,15 +15,15 @@ RSpec.describe Course, type: :model do
       expect(course.valid?).to eq(false)
     end
 
-  #  it "ensures the Course is active by default" do
-  #    Course = Course.new(body: "Content of the body", title: "Title")
-  #    expect(Course.active?).to eq(true)
-  #  end
+    it "ensures the Course is active by default" do
+      course = Course.new(body: "Content of the body", title: "Title")
+      expect(course.active?).to eq(true)
+    end
 
-  #  it "should be able to save Course" do
-  #    Course = Course.new(body: "Content of the body", title: "Title")
-  #    expect(Course.save).to eq(true)
-  #  end
+    it "should be able to save Course" do
+      course = Course.new(body: "Content of the body", title: "Title")
+      expect(Course.save).to eq(true)
+    end
   end
 
   #context "scopes tests" do
