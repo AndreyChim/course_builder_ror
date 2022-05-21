f7e3b0c00e2af7b265891e4bc21c5c8580a5020c
AndreyChim <chimitcyrenov@mail.ru>
2022-05-21 13:48:40 +0800

    add scope specs & validators

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
+
 RSpec.describe Course, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations tests" do
+    it "ensures the title is present" do
+      Course = Course.new(body: "Content of the body")
+      expect(Course.valid?).to eq(false)
+    end
+
+    it "ensures the body is present" do
+      Course = Course.new(title: "Title")
+      expect(Course.valid?).to eq(false)
+    end
+
+    it "ensures the Course is active by default" do
+      Course = Course.new(body: "Content of the body", title: "Title")
+      expect(Course.active?).to eq(true)
+    end
+
+    it "should be able to save Course" do
+      Course = Course.new(body: "Content of the body", title: "Title")
+      expect(Course.save).to eq(true)
+    end
+  end
+
+  context "scopes tests" do
+
+  end
 end
