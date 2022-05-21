45cac61aa681bfa62abed099bdb1789c322ea0ec
AndreyChim <chimitcyrenov@mail.ru>
2022-05-21 15:35:49 +0800

    add migrate MakeCourseActiveByDefault

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_044047) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_072731) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
