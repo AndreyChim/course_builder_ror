04da95d30157a6718b5195f7129746704a6aa430
AndreyChim <chimitcyrenov@mail.ru>
2022-05-21 13:55:52 +0800

    adelete row to pending add some ...

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
 
   context "validations tests" do
     it "ensures the title is present" do
