576f7cf2f180db2e569d116c887dc1461e2422ae
AndreyChim <chimitcyrenov@mail.ru>
2022-06-17 21:42:31 +0800

    add tests to units

--- spec/controllers/units_controller_spec.rb
+++ spec/controllers/units_controller_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe UnitsController, :type => :controller do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "routing" do
+
+    it { expect(put: "/courses/1/units/1").to route_to(controller: "units", action: "update", course_id: "1", id: "1") }
+  
+    it { expect(GET: "/courses/1/units/1").to route_to(controller: 'units', action: 'show', course_id: "1", id: "1") }
+  
+    it { expect(get: '/units').to route_to(controller: 'units', action: 'index') }
+  
+    end
 end
